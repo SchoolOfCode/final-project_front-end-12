679bfd42213845485215730c3e4e357c485463a8
codedresser <mr.matt.miller@gmail.com>
2022-07-26 10:52:41 +0100

    first commit - create next app boiler plate

---
+++ seasonal/pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>What is in season?</title>
+        <meta name="description" content="Seasonal Foods" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Hello World. Welcome to our awesome app!
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        THIS IS WHERE THE SEARCH BAR WILL GO!
+      </footer>
+    </div>
+  )
+}
