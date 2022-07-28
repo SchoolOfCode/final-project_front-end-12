36c4940af232c4ff70c02258c91ff8bf7327b2fa
N-LEE-94 <nathanlee42@btinternet.com>
2022-07-28 11:38:16 +0100

    burger menu toggle state

--- seasonal/components/burgerMenu.js
+++ seasonal/components/burgerMenu.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from '../styles/burger-menu.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export function BurgerMenu() {
-  
+  const [dropdown, setDropdown] = useState(false);
+
+  function toggleDropdown() {
+    // 👇️ passed function to setState
+    setDropdown(current => !current);
+  };
+
   return(
-    //icon for burger menu
-    //onClick, (toggles state to display: dropdown menu)
-    //  
+     
 
     <>
-      <Image src='/burgerMenu.png' className={styles.menu} alt='burger menu' height={50} width={50} />
+      <Image src='/burgerMenu.png' onClick={toggleDropdown} className={styles.menu} alt='burger menu' height={50} width={50} />
       
-      <div className={styles.dropdown}> 
-        <Link href="/aboutPage">About Page</Link>
-      </div>
+      {dropdown &&
+        <div className={styles.dropdown}> 
+          <Link href="/aboutPage">About Page</Link>
+        </div>
+      }
     {/* dropdown div should be rendered when the burger button is toggled.
     This requires */}
     </>
