8bf38109773917dc6f329271aff4c49d428e5058
madisonlowe <mcourteney@live.com>
2022-07-28 16:30:51 +0100

    CSS started for Card.js.

--- seasonal/components/Card.js
+++ seasonal/components/Card.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import styles from "../styles/Card.module.css";
 //Import for when using proper state (once it persists on route change)
 //import { useSearchContext } from "../context/search.js";
 
@@ -24,27 +25,31 @@ export default function Card() {
   //Import for when using proper state (once it persists on route change)
   //const [exampleState, setexampleState] = useSearchContext();
   return (
-    <div>
-      <h1>{exampleState.payload[0].name}</h1>
-      <ol>
+    <div className={styles.container}>
+      <h1 className={styles.header}>{exampleState.payload[0].name}</h1>
+      <ol className={styles.months}>
         {exampleState.payload[0].month.map((individualMonth, index) => {
           return <li key={index}>{individualMonth}</li>;
         })}
       </ol>
-      <ul>
+      <ul className={styles.foodTags}>
         <li>Family: {exampleState.payload[0].family}</li>
         <li>Food Type: {exampleState.payload[0].foodtype}</li>
         <li>Used As: {exampleState.payload[0].usedas}</li>
         <li>Allergens: {exampleState.payload[0].allergens}</li>
       </ul>
-      <Image
-        src='https://res.cloudinary.com/dvpop7e9w/image/upload/v1659018752/cld-sample-4.jpg'
-        className=''
-        alt='Image of food item'
-        width={300}
-        height={200}
-      />
-      <p>{exampleState.payload[0].description}</p>
+      <div className={styles.descriptionContainer}>
+        <Image
+          src="https://res.cloudinary.com/dvpop7e9w/image/upload/v1659018752/cld-sample-4.jpg"
+          alt="Image of food item"
+          width={300}
+          height={200}
+          className={styles.mainImage}
+        />
+        <p className={styles.mainDescription}>
+          {exampleState.payload[0].description}
+        </p>
+      </div>
     </div>
   );
 }
