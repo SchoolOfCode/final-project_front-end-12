7cd851dcf0f206961adebba106aeb05f52e0ed28
multi-vit <103585624+multi-vit@users.noreply.github.com>
2022-07-29 12:14:59 +0100

    Separate concerns so card does fetch and renders instead of searchbar

--- seasonal/components/Card.js
+++ seasonal/components/Card.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import styles from "../styles/Card.module.css";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 //Import for when using proper state (once it persists on route change)
-//import { useSearchContext } from "../context/search.js";
+import { useSearchContext } from "../context/search.js";
 
 const exampleState = {
   success: true,
@@ -21,37 +23,60 @@ const exampleState = {
   ],
 };
 
+const searchString = "https://seasonality-server-new.herokuapp.com/produce";
+
 export default function Card() {
-  //Import for when using proper state (once it persists on route change)
-  //const [exampleState, setexampleState] = useSearchContext();
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.header}>{exampleState.payload[0].name}</h1>
-      <ol className={styles.months}>
-        {exampleState.payload[0].month.map((individualMonth, index) => {
-          return <li key={index}>{individualMonth}</li>;
-        })}
-      </ol>
-      <ul className={styles.foodTags}>
-        <li>Family: {exampleState.payload[0].family}</li>
-        <li>Food Type: {exampleState.payload[0].foodtype}</li>
-        <li>Used As: {exampleState.payload[0].usedas}</li>
-        <li>Allergens: {exampleState.payload[0].allergens}</li>
-      </ul>
-      <div className={styles.descriptionContainer}>
-        <Image
-          src="https://res.cloudinary.com/dvpop7e9w/image/upload/v1659018752/cld-sample-4.jpg"
-          alt="Image of food item"
-          width={300}
-          height={200}
-          className={styles.mainImage}
-        />
-        <p className={styles.mainDescription}>
-          {exampleState.payload[0].description}
-        </p>
+  const router = useRouter();
+  const [currentItem, setCurrentItem] = useState({ payload: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(JSON.stringify(router.query));
+      const data = await fetch(`${searchString}?item=${router.query.food}`);
+      let result = await data.json();
+      setCurrentItem(result);
+      console.log(
+        `this is the search results console log: ${JSON.stringify(result)}`
+      );
+    };
+    if (!router.isReady) return;
+    fetchData()
+      // make sure to catch any error
+      .catch(console.error);
+  }, [router.isReady]);
+
+  if (currentItem.payload.length > 0) {
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.header}>{currentItem.payload[0].name}</h1>
+        <ol className={styles.months}>
+          {currentItem.payload[0].month.map((individualMonth, index) => {
+            return <li key={index}>{individualMonth}</li>;
+          })}
+        </ol>
+        <ul className={styles.foodTags}>
+          <li>Family: {currentItem.payload[0].family}</li>
+          <li>Food Type: {currentItem.payload[0].foodtype}</li>
+          <li>Used As: {currentItem.payload[0].usedas}</li>
+          <li>Allergens: {currentItem.payload[0].allergens}</li>
+        </ul>
+        <div className={styles.descriptionContainer}>
+          <Image
+            src='https://res.cloudinary.com/dvpop7e9w/image/upload/v1659018752/cld-sample-4.jpg'
+            alt='Image of food item'
+            width={300}
+            height={200}
+            className={styles.mainImage}
+          />
+          <p className={styles.mainDescription}>
+            {currentItem.payload[0].description}
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <p>Loading</p>;
+  }
 }
 
 /*
