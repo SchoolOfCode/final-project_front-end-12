09627324da95b94f41a38d353cca9f6d46b40d9c
fiona <fiona.kitchen@yahoo.co.uk>
2022-08-01 11:42:36 +0100

    waiting on fetch string- error message written and logic mostly written

--- seasonal/components/RandomFiveArray.js
+++ seasonal/components/RandomFiveArray.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from './Button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 
@@ -8,29 +8,38 @@ export function RandomFiveArray(){
 const [randomArray, setRandomArray]=useState({ payload: [] })
 const [timeStamp, setTimeStamp]=useState('')
 
+/* let dummydata= {payload: [{name:'carrot' },{name:'pear'},{name:'apple'}] }
+ */
 // we made usestates for the random array - this will be updated by the fetch 
 // we want to find out how to get timestamp data from a user when they press on teh site
 // we need to see what format that is in and how we pass it to the fetch request 
 
-
-useEffect(() => {
+ useEffect(() => {
     let date= Date()
+    console.log(date)
     let month= date.slice(4,7)
     setTimeStamp(month)
+
     const fetchData = async () => {
      // here we want to the fetch string the month 
-        
       const data = await fetch(``);
       let result = await data.json();
       setRandomArray(result);
       console.log(
-        `this is the search results console log: ${JSON.stringify(result)}`
-      );
+        `this is the search results console log: ${JSON.stringify(result)}`);
+    
+}
+
+fetchData()
    
-  },[timeStamp]);
+  },[]); 
 
+  if (randomArray.payload.length > 0) {
+    return randomArray.payload.map((result) => {
+      return <Button text={result.name} />;
+    });
+  } else {
+    return <p>Hmm, that's strange we know we put something here, please refresh to try again !</p>;
+  }
 
- return randomArray.payload.map((result) => {
-            return <Button text={result.name} />;
-          });
-        }
+}
