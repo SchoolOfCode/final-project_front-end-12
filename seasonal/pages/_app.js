4b7de863907fde6e98f142723dfa09cef381113d
fiona <fiona.kitchen@yahoo.co.uk>
2022-08-04 15:48:29 +0100

    starting and failing axe testing
    Co-authored-by: Andy Johnson <103585624+multi-vit@users.noreply.github.com> Co-authored-by: Madison Lowe <101978552+madisonlowe@users.noreply.github.com>

--- seasonal/pages/_app.js
+++ seasonal/pages/_app.js
@@ -1,7 +1,23 @@
 import "../styles/globals.css";
 import { SearchResultsProvider } from "../context/search.js";
+import ReactDOM from "react-dom";
+import React from "react";
+import { useEffect } from "react"
+import axe from "@axe-core/react";
+
+
 
 function MyApp({ Component, pageProps }) {
+
+
+  useEffect(() => {
+    
+    if (typeof window !== 'undefined' && process.env.NODE_ENV !== "production") {
+     
+      axe(React, ReactDOM, 1000);
+    }
+  }, []);
+
   return (
     <SearchResultsProvider>
       <Component {...pageProps} />
