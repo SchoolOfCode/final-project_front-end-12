47d1568b3078c30139b0bcfdab49e95143779bfe
multi-vit <103585624+multi-vit@users.noreply.github.com>
2022-08-07 14:03:11 +0100

    Solve link to current page doing nothing

--- seasonal/components/BurgerMenu.js
+++ seasonal/components/BurgerMenu.js
@@ -1,28 +1,45 @@
-import React, {useState} from "react";
-import styles from '../styles/BurgerMenu.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { useState } from "react";
+import styles from "../styles/BurgerMenu.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
-export function BurgerMenu() {
+export function BurgerMenu({ handleLinkClick }) {
   const [dropdown, setDropdown] = useState(false);
 
   function toggleDropdown() {
-    setDropdown(current => !current);
-  };
+    setDropdown((current) => !current);
+  }
 
-  return(
+  return (
     <>
       <div className={styles.wrapper}>
-      <Image src='/burgerIconMock.png' onClick={toggleDropdown} alt='burger menu' height={40} width={40} className={styles.menu} />
+        <Image
+          src='/burgerIconMock.png'
+          onClick={toggleDropdown}
+          alt='burger menu'
+          height={40}
+          width={40}
+          className={styles.menu}
+        />
       </div>
-      
-      {dropdown &&
-          <div className={styles.dropdown} aria-label="dropdown menu for navigation">
-              <Link href="/">Home</Link>
-              <Link href="/about">About</Link>
-              <Image src='/close.png' onClick={toggleDropdown} alt='close menu' height={20} width={20} className={styles.closeMenu} />
-          </div>
-      }
+
+      {dropdown && (
+        <div
+          className={styles.dropdown}
+          aria-label='dropdown menu for navigation'
+        >
+          <a onClick={(e) => handleLinkClick(e, "/")}>Home</a>
+          <a onClick={(e) => handleLinkClick(e, "/about")}>About</a>
+          <Image
+            src='/close.png'
+            onClick={toggleDropdown}
+            alt='close menu'
+            height={20}
+            width={20}
+            className={styles.closeMenu}
+          />
+        </div>
+      )}
     </>
-  )
-};
+  );
+}
