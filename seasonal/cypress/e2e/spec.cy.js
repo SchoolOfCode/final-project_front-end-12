c5022e07c8b2ad40f0b2b6958d9320162a95b489
madisonlowe <mcourteney@live.com>
2022-08-08 16:15:31 +0100

    Completed more Cypress tests.
    Co-authored-by: Nathan Lee <100144036+N-LEE-94@users.noreply.github.com>
    Co-authored-by: Fiona Kitchen <100845392+fkit00@users.noreply.github.com>

--- seasonal/cypress/e2e/spec.cy.js
+++ seasonal/cypress/e2e/spec.cy.js
@@ -10,9 +10,82 @@ describe("Typical user journey through our website", () => {
     cy.url().should("include", "results?month=");
   });
 
-  /*   it("Clicks on Banana (which is always in season) and checks we have been redirected to the item page", () => {
+  it("Clicks on Banana (which is always in season) and checks we have been redirected to the item page", () => {
     cy.wait(1000);
     cy.contains("Banana").click();
     cy.url().should("include", "item?food=Banana");
-  }); */
+  });
 });
+
+describe("Test that the RandomFiveArray works as expected", () => {
+  it("Returns five cards on the page", () => {
+    cy.visit("http://localhost:3000");
+    cy.wait(1000);
+    cy.get("button").should("have.length", 7);
+  });
+  // please complete below test teehee
+  it("Returns specifically five different things in the array", () => {
+    cy.visit("http://localhost:3000");
+    cy.wait(1000);
+    cy.get(".randomarray");
+    // visit the page
+    // perceive the page
+    // perceive that there are five cards upon the page
+    // perceive that these five cards, all alike in dignity, in fair seasonalfood app where we lay our scene, are different
+    // profit x2
+  });
+});
+
+describe("Test that searching for terms behaves as expected", () => {
+  it("Returns a correct search for an item", () => {
+    cy.visit("http://localhost:3000");
+    cy.wait(1000);
+    cy.get("input").type("pear");
+    cy.get("select").select("Item");
+    cy.contains("Submit").click();
+    cy.url().should("include", "item?food=pear");
+    cy.wait(1000);
+    cy.get("h2").contains("Pear");
+  });
+
+  it("Returns a correct search for a month", () => {
+    cy.visit("http://localhost:3000");
+    cy.wait(1000);
+    cy.get("input").type("january");
+    cy.get("select").select("Month");
+    cy.contains("Submit").click();
+    cy.url().should("include", "results?month=january");
+    cy.wait(1000);
+    cy.contains("Apple").click().wait(1000);
+    cy.get("h2").contains("Apple");
+    cy.get("ol").children().contains("January");
+  });
+
+  it("Returns a correct error message for an incorrect search where wrong filter is selected", () => {
+    cy.visit("http://localhost:3000");
+    cy.wait(1000);
+    cy.get("input").type("january");
+    cy.get("select").select("Item");
+    cy.contains("Submit").click();
+    cy.url().should("include", "item?food=january");
+    cy.wait(1000);
+    cy.get("p").contains("Loading");
+  });
+
+  it.only("Returns a correct error message for an incorrect search where numbers are entered", () => {
+    cy.visit("http://localhost:3000");
+    cy.wait(1000);
+    cy.get("input").type("12345");
+    cy.get("select").select("Item");
+    cy.contains("Submit").click();
+    cy.wait(1000);
+    cy.on("window:alert", (txt) => {
+      expect(txt).to.contains("Please use letters only in your search");
+    });
+  });
+});
+
+// WHAT NEEDS DOING
+// test the burger icon links work (make sure clicking on each one works and routes properly)
+// test the random five array works (done-ish, need to finish line 26)
+// test recipe user journey if completed?
