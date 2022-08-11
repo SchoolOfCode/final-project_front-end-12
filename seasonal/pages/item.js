e8d4a55b03d7ad586f039d383bb7c18dddcec732
N-LEE-94 <nathanlee42@btinternet.com>
2022-08-11 09:32:35 +0100

    fixed case error for netlify deploy

--- seasonal/pages/item.js
+++ seasonal/pages/item.js
@@ -12,6 +12,9 @@ export default function Item() {
         <title>Seasonal</title>
         <meta name="description" content="Individual item page" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
       </Head>
       <NavBar aria-label="Navigation"/>
       <Card />
