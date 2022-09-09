5747653996d73e6595f80af6c42040cf9af0de0f
madisonlowe <mcourteney@live.com>
2022-09-09 11:50:55 +0100

    Updated CSS.
    Co-authored-by: Fiona Kitchen <100845392+fkit00@users.noreply.github.com>
    Co-authored-by: Andy Johnson <103585624+multi-vit@users.noreply.github.com>

--- seasonal/components/ResultsItem.js
+++ seasonal/components/ResultsItem.js
@@ -11,22 +11,20 @@ export function ResultsItem() {
 
   useEffect(() => {
     const fetchData = async () => {
-      if(router.query.month !== undefined )
-      {
-      let modifiedMonth = router.query.month;
-      console.log(`this is the month= ${modifiedMonth}`)
-      const data = await fetch(`${searchString}?month=${modifiedMonth}`);
-      let result = await data.json();
-      setSearchResults(result);
-      }
-   else
-      {
+      if (router.query.month !== undefined) {
+        let modifiedMonth = router.query.month;
+        console.log(`this is the month= ${modifiedMonth}`);
+        const data = await fetch(`${searchString}?month=${modifiedMonth}`);
+        let result = await data.json();
+        setSearchResults(result);
+      } else {
         let allergen = router.query.allergen;
-        console.log(`this is the allergen = ${allergen}`)
+        console.log(`this is the allergen = ${allergen}`);
         const data = await fetch(`${searchString}?allergen=${allergen}`);
         let result = await data.json();
         setSearchResults(result);
-        }
+        console.log(`this is the result: ${JSON.stringify(result)}`);
+      }
     };
     if (!router.isReady) return;
     fetchData()
